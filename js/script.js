f8d11b27ac04db25cb1773cf16d046604c4a7979
Y39WebDeveloper <95289571+Y39WebDeveloper@users.noreply.github.com>
2022-04-26 16:15:31 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ let spans = document.querySelectorAll(".progress span");
 
 window.onscroll = function () {
     if (window.scrollY >= section1.offsetTop - 100) {
-        spans.forEach((span) => {
-            span.style.width = span.dataset.width;
+        spans.forEach((spans) => {
+            spans.style.width = spans.dataset.width;
         });
     };
 };
